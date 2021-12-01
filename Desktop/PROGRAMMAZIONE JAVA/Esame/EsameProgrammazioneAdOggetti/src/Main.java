b10f30aa7487dfd12add466ca1a041f96dc410da
AlessandroS01 <seghini53@gmail.com>
2021-12-01 18:36:20 +0100

    Main vuoto

--- Desktop/PROGRAMMAZIONE JAVA/Esame/EsameProgrammazioneAdOggetti/src/Main.java
+++ Desktop/PROGRAMMAZIONE JAVA/Esame/EsameProgrammazioneAdOggetti/src/Main.java
@@ -2,15 +2,5 @@
 public class Main {
 		
 	public static void main (String[] Args) {
-		System.out.println("Hello World!");
-		System.out.println(15);
-		System.out.println("PORVAAAAAAAAAAAAAAA");
-		System.out.println("AIUTOOOOOOOOOOOOOOOOOOOOOO");
-		System.out.println("AAAAAAAAAAAAAAAa");
-		System.out.println("AAAAAAAAAAAAAAAa");
-		System.out.println("AAAAAAAAAAAAAAAa");
-		System.out.println("AAAAAAAAAAAAAAAa");
-		System.out.println("AAAAAAAAAAAAAAAa");
-		System.out.println("AAAAAAAAAAAAAAAa");
 	}
 }
