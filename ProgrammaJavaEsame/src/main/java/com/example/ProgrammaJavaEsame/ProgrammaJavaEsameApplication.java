2fff4160df3e0f07b33526e015d54b835c9dbd15
manuelmulloni <95358949+manuelmulloni@users.noreply.github.com>
2021-12-08 10:48:02 +0100

    n

--- ProgrammaJavaEsame/src/main/java/com/example/ProgrammaJavaEsame/ProgrammaJavaEsameApplication.java
+++ ProgrammaJavaEsame/src/main/java/com/example/ProgrammaJavaEsame/ProgrammaJavaEsameApplication.java
@@ -8,12 +8,7 @@ public class ProgrammaJavaEsameApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProgrammaJavaEsameApplication.class, args);
-<<<<<<< HEAD
-		int t = 0;
-		
-=======
-		System.out.println("CIAO");
->>>>>>> branch 'master' of https://github.com/AlessandroS01/ProgettoJava
+        int t = 0;
 	}
 
 }
