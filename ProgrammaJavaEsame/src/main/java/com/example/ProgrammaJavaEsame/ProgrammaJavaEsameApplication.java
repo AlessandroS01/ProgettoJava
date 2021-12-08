0d1cb6810eba7d49c93aba5822956e0d27ca8ab8
AlessandroS01 <seghini53@gmail.com>
2021-12-08 10:08:54 +0100

    first commit

---
+++ ProgrammaJavaEsame/src/main/java/com/example/ProgrammaJavaEsame/ProgrammaJavaEsameApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProgrammaJavaEsame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgrammaJavaEsameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgrammaJavaEsameApplication.class, args);
+	}
+
+}
