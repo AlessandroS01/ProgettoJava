6a0d73a59c0dfd6a58481f58ed6272c981102885
AlessandroS01 <seghini53@gmail.com>
2021-12-11 12:15:19 +0100

    changed name to a package which resulted to be outside the main one

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/ProgettoOopApplication.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/ProgettoOopApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ProgettoOopApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ProgettoOopApplication.class, args);
+		SpringApplication.run(ProgettoOopApplication.class, args); 
 	}
 
 }
