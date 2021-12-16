be4ed86c4bf682d21b44cebda91fdda8b64a00bc
AlessandroS01 <seghini53@gmail.com>
2021-12-16 16:36:04 +0100

    getWeatherDaily fatto insieme a lezione

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/Services/StartConnectionURL.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/Services/StartConnectionURL.java
@@ -6,10 +6,8 @@ import java.net.*;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-
 import org.json.simple.*;
-
-import it.univpm.ProgettoOOP.Model.Weather;
+import org.json.*;
 
 
 /** This class is used as a model extended by :
@@ -22,11 +20,6 @@ public class StartConnectionURL {
 	 * @param Key is used to generate the API.
 	 */
 	protected final String Key = "ef3288292630c5a8c91c5b45de242182";
-	
-
-	public Weather startConnection ( String city ) {
-		return null;
-	}
 	
 }
 	
--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/Services/StartConnectionURL5Days.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/Services/StartConnectionURL5Days.java
@@ -1,16 +1,18 @@
 package it.univpm.ProgettoOOP.Services;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
-import java.net.http.HttpClient;
-import java.net.http.HttpRequest;
-import java.net.http.HttpResponse;
+import java.util.Map;
 
 import org.json.simple.JSONObject;
-
+import org.json.*;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import it.univpm.ProgettoOOP.Model.Weather;
 
@@ -31,13 +33,34 @@ public class StartConnectionURL5Days extends StartConnectionURL {
 	 * @return 
 	*/
 	
-	@Override
-	public Weather startConnection ( String city) {
-		HttpClient client = HttpClient.newHttpClient();
-		HttpRequest request = HttpRequest .newBuilder().uri(URI.create("http://" + url5Days + "q=" + city + "&appid" + Key)).build();
-		HttpResponse response = (HttpResponse) client.sendAsync(request, HttpResponse.BodyHandlers.ofString());
-		Parsing parse = new Parsing ();
-		return parse.parseWindTemp(response);
+	public JSONObject getWeatherDaily ( String city) {
+		OkHttpClient client = new OkHttpClient();
+		Request request =new Request.Builder().url("http://" + url5Days + "q=" + city + "&appid" + Key).build();
+		try{
+		Response response = client.newCall(request).execute();
+		JSONObject caso;
+		caso = new JSONObject(response.body().string());
+		return JSONObject( response.body().string());
+		} catch (IOException e ) {
+			e.printStackTrace();
+		}
+		
+		return null;
+		/*	HttpRequest request = HttpRequest .newBuilder().uri(URI.create("http://" + url5Days + "q=" + city + "&appid" + Key)).build();
+		HttpResponse response;
+		try{
+			response =  client.send(request,  HttpResponse.BodyHandlers.ofString());
+			return new JSONObject(s);
+		}catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+		*/
+		//Parsing parse = new Parsing ();
+		//return parse.parseWindTemp(response);
 		
 		/**
 		   client.sendAsync(request, HttpResponse.BodyHandlers.ofString())
