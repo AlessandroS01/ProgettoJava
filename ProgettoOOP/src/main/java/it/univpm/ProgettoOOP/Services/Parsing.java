f3820b317f9af446944ee71de410ba7c0723d841
manuelmulloni <95358949+manuelmulloni@users.noreply.github.com>
2021-12-27 11:29:34 +0100

    modifiche

--- ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/Services/Parsing.java
+++ ProgettoOOP/src/main/java/it/univpm/ProgettoOOP/Services/Parsing.java
@@ -1,6 +1,5 @@
 package it.univpm.ProgettoOOP.Services;
-import java.util.Timer;
-import java.util.TimerTask; 
+ 
 
 import java.net.http.HttpResponse;
 import java.util.Map;
@@ -15,63 +14,13 @@ import it.univpm.ProgettoOOP.Model.Weather;
  * Class that parse and instantiate the values 
  * written in the API in our model
  */
-public class Parsing {
-	
-	
-	
-	Timer timer;
-	
-	
-	public Parsing() {
-		timer = new Timer();
-		timer.schedule(new RemindTask(),3600000);
-	} 
-	class RemindTask extends TimerTask {
-  boolean time2 = true;
-		@Override
-		public void run() {
-		while(time2) {
-	         	StartConnectionURLDaily daily = new StartConnectionURLDaily("ancona");
-				
-				JSONObject Obj = daily.startConnectionDaily();
-				JSONObject ObjWind = (JSONObject) Obj.get("wind");
-				JSONArray Arr = new JSONArray(); 
-				
-				Arr = (JSONArray) Obj.get("weather");
-				
-				String state = (String) Obj.get("country");
-				String nameCity = (String) Obj.get("name");
-				
-				Long date = (Long) Obj.get("dt");
-				java.util.Date time = new java.util.Date((long)date*1000);
-				
-				float speed = (float) ObjWind.get("wind");
-				int deg = (int) ObjWind.get("deg");
-				float gust = (float) ObjWind.get("gust");
-				
-				String weather = (String) Arr.get(2);
-				
-				Weather weatSupport = new Weather ( time , speed , deg , gust , weather);
-				Vector <Weather> weatVec = new Vector <Weather>();
-				weatVec.add(weatSupport);
-				
-				Place weatherXPlaceDaily = new Place ( state , nameCity , weatVec);
-				 weatherXPlaceDaily.getCity(); 
-				 weatherXPlaceDaily.getWeatherXTime();
-				 weatherXPlaceDaily.getState();
-				 
-			}
-			
-			
-			
-		}
-			
-			
-			
-		}
+
+		
+		
+	         	
 		   
 		   
-	   }
+	   
 	/* 
 	 * 
 	 * timer.schedule(parsingConnDaily, 3600000); // esempio
@@ -83,7 +32,8 @@ public class Parsing {
 	 * Method that parse the values written inside the
 	 * currentWeather API
 	 */
-/*	public Place parsingConnDaily( JSONObject obj , String city) {
+	 public class Parsing {
+     public Place parsingConnDaily( JSONObject obj , String city) {
 		
 		StartConnectionURLDaily daily = new StartConnectionURLDaily( city);
 		
@@ -121,6 +71,6 @@ public class Parsing {
 	
 
 
-} */ 
-   
+} 
+  
    
